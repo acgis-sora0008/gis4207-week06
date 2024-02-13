d8efc0c8d2f044f35db2911567ee727f28c9ab51
acgis-amat0007 <amat0007@algonquinlive.com>
2024-02-13 13:56:45 -0500

    Outline

---
+++ MousmiS_AntoninoA/Batch_clipper.py
@@ -0,0 +1,44 @@
+import sys
+from arcpy import env
+
+
+
+def main():
+    # if name in_feat not exist (WS)
+
+
+    #if clip_Feat not exist (WS)
+
+    # if out_feat not esist  (WS)
+
+    import arcpy
+
+    print ('not done')
+
+
+
+
+
+
+
+
+
+
+
+
+def batch_clipper(in_features, clip_features, out_feature_class):
+         
+# for each clip_feature:
+        # clip by each in_feature 
+        # arcpy.clip
+        # output = relative path
+        # name = ClipWS_InWS
+
+
+
+        # arcpy.env.overwriteOutput = True
+        print ('not done')
+
+    
+if __name__ == "__main__":
+    main()
