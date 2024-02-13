a58849b6d986364f314eba7cafa0c2e90151c56c
acgis-amat0007 <amat0007@algonquinlive.com>
2024-02-13 14:54:00 -0500

    AA

--- MousmiS_AntoninoA/Batch_clipper.py
+++ MousmiS_AntoninoA/Batch_clipper.py
@@ -29,16 +29,22 @@ def main():
     env.workspace = in_workspace
     env.overwriteOutput = True
 
-    in_fc = arcpy.ListFeatureClasses("", "", in_workspace)
-    print (in_fc)
-    clip_fc = arcpy.ListFeatureClasses("", "", clip_workspace)
+    in_fcs = arcpy.ListFeatureClasses()
+    for in_fc in in_fcs:
+        in_feat_list = []
+        in_feat_list.append(os.path.abspath(os.path.join(in_workspace, in_fc)))
+        print(in_feat_list)
+        # print(in_fc)
+
+    sys.exit()
+    clip_fc = arcpy.ListFeatureClasses()
     print (clip_fc)
     
     for in_feat in in_fc:
         for clip_feat in clip_fc:
             out_fc_name = f"{os.path.splitext(clip_feat)[0]}_{os.path.splitext(in_feat)[0]}.shp"
             out_fc = os.path.join(output_workspace, out_fc_name)
-            arcpy.Clip_analysis(in_feat, clip_feat, out_fc)
+            # arcpy.Clip_analysis(in_faet, clip_feat, out_fc)
             print (f'{out_fc} done')
 
 
