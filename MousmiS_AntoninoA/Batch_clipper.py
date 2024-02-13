120b34e4843d6cc9f6ea328fd93157cce624f77a
acgis-amat0007 <amat0007@algonquinlive.com>
2024-02-13 14:26:15 -0500

    AA + MS

--- MousmiS_AntoninoA/Batch_clipper.py
+++ MousmiS_AntoninoA/Batch_clipper.py
@@ -1,42 +1,46 @@
 import sys
 import os
-import arcpy
-from arcpy import env
-
 
 
 def main():
-    # if name in_feat not exist (WS)
+    if len(sys.argv) != 4:
+        print('Usage: batch_clipper.py <InWorkspace> <ClipWorkspace> <OutputWorkspace>')
+        sys.exit()
 
+    in_workspace = sys.argv[1]
+    clip_workspace = sys.argv[2]
+    output_workspace = sys.argv[3]
 
-    #if clip_Feat not exist (WS)
+    if not os.path.exists(in_workspace):
+        print(f"{in_workspace} does not exist.")
+        sys.exit()
 
-    # if out_feat not esist  (WS)
+    if not os.path.exists(clip_workspace):
+        print(f"{clip_workspace} does not exist.")
+        sys.exit()
+
+    if not os.path.exists(output_workspace):
+        print(f"{output_workspace} does not exist.")
+        sys.exit()
 
     import arcpy
+    from arcpy import env
 
-    print ('not done')
+    env.workspace = in_workspace
+    env.overwriteOutput = True
 
-
-
-
-
-
-
-
-
-
-
-
-def batch_clipper(in_features, clip_features, out_feature_class):
-    for in_fc in in_features:
-        for clip_fc in clip_features:
-            out_fc_name = "{}_{}.shp".format(os.path.splitext(clip_fc)[0], os.path.splitext(in_fc)[0])
-            out_fc = os.path.join(out_feature_class,out_fc_name)
-            arcpy.Clip_analysis(in_fc, clip_fc, out_fc)
-            arcpy.env.overwriteOutput = True
-
-
+    in_fc = arcpy.ListFeatureClasses("", "", in_workspace)
+    print (in_fc)
+    clip_fc = arcpy.ListFeatureClasses("", "", clip_workspace)
+    print (clip_fc)
     
+    for in_feat in in_fc:
+        for clip_feat in clip_fc:
+            out_fc_name = f"{os.path.splitext(clip_feat)[0]}_{os.path.splitext(in_feat)[0]}.shp"
+            out_fc = os.path.join(output_workspace, out_fc_name)
+            arcpy.Clip_analysis(in_feat, clip_feat, out_fc)
+            print (f'{out_fc} done')
+
+
 if __name__ == "__main__":
     main()
